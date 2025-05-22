9fe9d8256852060c064ad3f1c6ffb34ad054a7e3
Mourat Achmet oglou Iliaz <mayrit91@gmail.com>
2025-05-22 14:57:51 +0300

    Java Master Class: 123 Descending Sort Challenge is added

---
+++ JavaMasterClassDescendingSortChallenge/src/src/Main.java
@@ -0,0 +1,28 @@
+package src;
+
+import java.lang.reflect.Array;
+import java.util.Arrays;
+import java.util.Random;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Random r = new Random();
+		int[] array = r.ints(10, 0, 100).toArray();
+		
+		System.out.println(Arrays.toString(array));
+		
+		Arrays.sort(array);
+		
+		System.out.println(Arrays.toString(array));
+		
+		int[] desArray = new int[array.length];
+		for(int i = 0; i < array.length; i++) {
+			desArray[i] = array[array.length - 1 - i];
+		}
+		
+		System.out.println(Arrays.toString(desArray));
+	}
+
+}
