07b4d9df3710af0cbb13d6352e6b529836349d42
Mourat Achmet oglou Iliaz <mayrit91@gmail.com>
2025-05-22 18:19:30 +0300

    Java Master Class: 128 Reverse Array Challenge is added

--- JavaMasterClassMinimumElementChallenge/src/src/Main.java
+++ JavaMasterClassMinimumElementChallenge/src/src/Main.java
@@ -4,33 +4,35 @@ import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
-	
+
 	public static void main(String[] args) {
 		int[] ns = readIntegers();
 		System.out.println(Arrays.toString(ns));
 		System.out.println(findMin(ns));
 	}
-	
+
 	public static int[] readIntegers() {
 		Scanner scanner = new Scanner(System.in);
 		String[] numberStrings = scanner.nextLine().split(",");
 		int[] numbers = new int[numberStrings.length];
-		
-		for(int i = 0; i < numbers.length; i++) {
-			numbers[i] = Integer.parseInt(numberStrings[i]);
+
+		for (int i = 0; i < numbers.length; i++) {
+			numbers[i] = Integer.parseInt(numberStrings[i].trim());
 		}
-		
+
 		scanner.close();
 		return numbers;
 	}
-	
+
 	public static int findMin(int[] array) {
+
 		int min = Integer.MAX_VALUE;
-		
-		for(int n : array) {
-			if(n < min) min = n;
+
+		for (int n : array) {
+			if (n < min)
+				min = n;
 		}
-		
+
 		return min;
 	}
 }
---
+++ JavaMasterClassReverseArrayChallenge/src/src/Main.java
@@ -0,0 +1,26 @@
+package src;
+
+import java.util.Arrays;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int[] numbers = {10, 2, 32, 11, 44, 24, 3};
+		
+		System.out.println(Arrays.toString(numbers));
+		reverseArray(numbers);
+		System.out.println(Arrays.toString(numbers));
+	}
+	
+	public static void reverseArray(int[] array) {
+		int temp;
+		
+		for(int i = 0; i < array.length/2; i++) {
+			temp  = array[i];
+			array[i] = array[array.length - 1 - i];
+			array[array.length - 1 - i] = temp;
+		}
+	}
+
+}
